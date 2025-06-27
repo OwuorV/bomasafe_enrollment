6262be274843d9dd63314930ba21bc5a5f08c360
OwuorV <myvinmail591@gmail.com>
2025-06-27 15:46:39 +0300

    another commit

--- app/(enrollment)/layout.tsx
+++ app/(enrollment)/layout.tsx
@@ -1,13 +1,9 @@
 import React from "react";
-type NavLink = {
-  label: string;
-  href: string;
-};
-interface LayoutProps {
+
+export default function Layout({
+  children,
+}: Readonly<{
   children: React.ReactNode;
-  navLinks: NavLink[];
-  logo: React.ReactNode;
-}
-export default function Layout({ children, navLinks, logo }: LayoutProps) {
+}>) {
   return <main>{children}</main>;
 }
